1e07360f0f9dcb79dde2a70b38db5d655ec664f1
chnoch@gmail.com <deadhorse3125>
2014-07-11 22:34:29 +0200

    added basic visualization option to baseline model

--- ModelLibrary/BaselineModel/src/ch/chnoch/mt/baselinemodel/BaselineModel.groovy
+++ ModelLibrary/BaselineModel/src/ch/chnoch/mt/baselinemodel/BaselineModel.groovy
@@ -3,10 +3,21 @@
  */
 package ch.chnoch.mt.baselinemodel
 
+import com.tinkerpop.blueprints.Edge
 import com.tinkerpop.blueprints.Graph
+import com.tinkerpop.blueprints.Vertex
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph
 import com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory
+import com.tinkerpop.blueprints.oupls.jung.GraphJung
 import com.tinkerpop.gremlin.groovy.Gremlin
+import edu.uci.ics.jung.algorithms.layout.CircleLayout
+import edu.uci.ics.jung.graph.event.GraphEvent
+import edu.uci.ics.jung.visualization.BasicVisualizationServer
+import edu.uci.ics.jung.visualization.renderers.Renderer
+import org.apache.commons.collections15.Transformer
+
+import javax.swing.JFrame
+import java.awt.Dimension
 
 class BaselineModel {
     def graph
@@ -49,6 +60,31 @@ class BaselineModel {
     }
 
     def printGraph() {
-        println graph
+        def g = new GraphJung(graph);
+        def layout = new CircleLayout<Vertex, Edge>(g);
+        layout.setSize(new Dimension(300, 300));
+        def viz = new BasicVisualizationServer<Vertex, Edge>(layout);
+        viz.setPreferredSize(new Dimension(350, 350));
+
+        def vertexLabelTransformer = new Transformer<Vertex, String>() {
+            public String transform(Vertex vertex) {
+                return (String) vertex.getProperty("name");
+            }
+        };
+
+        def edgeLabelTransformer = new Transformer<Edge, String>() {
+            public String transform(Edge edge) {
+                return edge.getLabel();
+            }
+        };
+
+        viz.getRenderContext().setEdgeLabelTransformer(edgeLabelTransformer);
+        viz.getRenderContext().setVertexLabelTransformer(vertexLabelTransformer);
+
+        def frame = new JFrame("TinkerPop");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(viz);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
