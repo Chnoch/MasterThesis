5a911d5e18c4f1f7140f124c57cebea93f816a6f
chnoch@gmail.com <deadhorse3125>
2014-07-21 17:03:44 +0200

    removed lots of unnecessary libraries

--- ModelLibrary/BaselineModel/src/ch/chnoch/mt/baselinemodel/BaselineModel.groovy
+++ ModelLibrary/BaselineModel/src/ch/chnoch/mt/baselinemodel/BaselineModel.groovy
@@ -4,21 +4,8 @@
 package ch.chnoch.mt.baselinemodel
 
 import ch.chnoch.mt.model.ModelInterface
-import com.tinkerpop.blueprints.Edge
-import com.tinkerpop.blueprints.Graph
-import com.tinkerpop.blueprints.Vertex
 import com.tinkerpop.blueprints.impls.tg.TinkerGraph
-import com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory
-import com.tinkerpop.blueprints.oupls.jung.GraphJung
 import com.tinkerpop.gremlin.groovy.Gremlin
-import edu.uci.ics.jung.algorithms.layout.CircleLayout
-import edu.uci.ics.jung.graph.event.GraphEvent
-import edu.uci.ics.jung.visualization.BasicVisualizationServer
-import edu.uci.ics.jung.visualization.renderers.Renderer
-import org.apache.commons.collections15.Transformer
-
-import javax.swing.JFrame
-import java.awt.Dimension
 
 class BaselineModel implements ModelInterface {
     def graph
@@ -88,31 +75,31 @@ class BaselineModel implements ModelInterface {
     }
 
     def printGraph() {
-        def g = new GraphJung(graph);
-        def layout = new CircleLayout<Vertex, Edge>(g);
-        layout.setSize(new Dimension(300, 300));
-        def viz = new BasicVisualizationServer<Vertex, Edge>(layout);
-        viz.setPreferredSize(new Dimension(350, 350));
-
-        def vertexLabelTransformer = new Transformer<Vertex, String>() {
-            public String transform(Vertex vertex) {
-                return (String) vertex.getProperty("name");
-            }
-        };
-
-        def edgeLabelTransformer = new Transformer<Edge, String>() {
-            public String transform(Edge edge) {
-                return edge.getProperty("count");
-            }
-        };
-
-        viz.getRenderContext().setEdgeLabelTransformer(edgeLabelTransformer);
-        viz.getRenderContext().setVertexLabelTransformer(vertexLabelTransformer);
-
-        def frame = new JFrame("TinkerPop");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(viz);
-        frame.pack();
-        frame.setVisible(true);
+//        def g = new GraphJung(graph);
+//        def layout = new CircleLayout<Vertex, Edge>(g);
+//        layout.setSize(new Dimension(300, 300));
+//        def viz = new BasicVisualizationServer<Vertex, Edge>(layout);
+//        viz.setPreferredSize(new Dimension(350, 350));
+//
+//        def vertexLabelTransformer = new Transformer<Vertex, String>() {
+//            public String transform(Vertex vertex) {
+//                return (String) vertex.getProperty("name");
+//            }
+//        };
+//
+//        def edgeLabelTransformer = new Transformer<Edge, String>() {
+//            public String transform(Edge edge) {
+//                return edge.getProperty("count");
+//            }
+//        };
+//
+//        viz.getRenderContext().setEdgeLabelTransformer(edgeLabelTransformer);
+//        viz.getRenderContext().setVertexLabelTransformer(vertexLabelTransformer);
+//
+//        def frame = new JFrame("TinkerPop");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.getContentPane().add(viz);
+//        frame.pack();
+//        frame.setVisible(true);
     }
 }
